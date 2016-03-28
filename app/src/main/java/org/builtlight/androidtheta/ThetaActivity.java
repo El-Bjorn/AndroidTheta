db9e1c5b78c5174cd8501cd55bf0225c6a12e34b
Bjorn Chambless <bjorn@builtlight.org>
2016-03-28 11:56:27 -0700

    commit #1

---
+++ app/src/main/java/org/builtlight/androidtheta/ThetaActivity.java
@@ -0,0 +1,13 @@
+package org.builtlight.androidtheta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThetaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theta);
+    }
+}
---
+++ app/src/main/java/org/builtlight/androidtheta/ThetaSession.java
@@ -0,0 +1,7 @@
+package org.builtlight.androidtheta;
+
+/**
+ * Created by bjorn on 3/28/16.
+ */
+public class ThetaSession {
+}
