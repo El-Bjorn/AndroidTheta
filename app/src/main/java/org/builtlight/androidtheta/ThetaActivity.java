551ce986bc43d4a21e9c778fe23bd5b9a2fb05cb
bjorn <bjorn@platypus.(none)>
2016-04-03 15:47:25 -0700

    Connect button and spinner. Added butterknife. No more actionbar

--- app/src/main/java/org/builtlight/androidtheta/ThetaActivity.java
+++ app/src/main/java/org/builtlight/androidtheta/ThetaActivity.java
@@ -3,18 +3,72 @@ package org.builtlight.androidtheta;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+//import android.content.Context;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class ThetaActivity extends AppCompatActivity {
     public static final String TAG = "THETA_ACTIVITY";
+
+    @InjectView(R.id.createSessionButton) Button createSessButton;
+    @InjectView(R.id.connectProgressBar) ProgressBar connectProgress;
+
+    //private ThetaSession mThetaSession;
+    //private Context mOurContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theta);
+        ButterKnife.inject(this);
 
         final ThetaSession thetaSession = new ThetaSession(this);
-        //Log.d.(thetaSession);
-        Log.d(TAG, "new ThetaSession");
+
+        Log.d(TAG, "Started Theta");
+        //mOurContext = this;
+
+        connectProgress.setVisibility(View.INVISIBLE);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connectProgress.setVisibility(View.VISIBLE);
+                //mThetaSession = new ThetaSession(mOurContext);
+                thetaSession.getCameraInfo();
+                thetaSession.startThetaSession(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (thetaSession.hasThetaSession) {
+                            //System.out.printf("got theta session %s\n", thetaSession.mSessionId);
+                            Log.d(TAG,"got theta session id: "+thetaSession.mSessionId);
+                            thetaSession.takePicture();
+
+                        } else {
+                            Log.d(TAG,"no theta session id for you");
+                            //System.out.printf("no theta sessions");
+                        }
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                connectProgress.setVisibility(View.INVISIBLE);
+                            }
+                        });
+
+                    }
+                });
+
+            }
+        };
+        createSessButton.setOnClickListener(listener);
+
+
+
+        /*Log.d(TAG, "new ThetaSession");
+
         thetaSession.getCameraInfo();
         thetaSession.startThetaSession(new Runnable() {
             @Override
@@ -29,6 +83,6 @@ public class ThetaActivity extends AppCompatActivity {
                     //System.out.printf("no theta sessions");
                 }
             }
-        });
+        }); */
     }
 }
